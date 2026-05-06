9e75b5e7d0ed2d50676808339350f9576e200da7
Travis Cline <travis.cline@gmail.com>
2026-05-05 21:02:37 -0700

    authorization: watchdog privileged execute
    Run AuthorizationExecuteWithPrivileges under the same watchdog budget as AuthorizationCreate and refuse native authorization from the registered UI thread.

--- authorization_test.go
+++ authorization_test.go
@@ -95,6 +95,39 @@ func TestRunElevatedManifestNativeTimesOutWithPromptPending(t *testing.T) {
 	}
 }
 
+func TestRunElevatedManifestNativeTimesOutExecuteWithoutPrompt(t *testing.T) {
+	restore := stubAuthorizationExecute(t, false)
+	defer restore()
+
+	err := runElevatedManifestNative("/tmp/manifest.json", "abc123", "Provision VM")
+	if err == nil {
+		t.Fatal("runElevatedManifestNative succeeded, want execute timeout")
+	}
+	if !strings.Contains(err.Error(), "AuthorizationExecuteWithPrivileges wedged after") {
+		t.Fatalf("error = %v, want AuthorizationExecuteWithPrivileges timeout", err)
+	}
+}
+
+func TestRunElevatedManifestNativeRefusesUIThread(t *testing.T) {
+	oldTTY := authorizationStdinIsTTY
+	oldUIThreadID := uiThreadID.Load()
+	t.Cleanup(func() {
+		authorizationStdinIsTTY = oldTTY
+		uiThreadID.Store(oldUIThreadID)
+	})
+
+	authorizationStdinIsTTY = func() bool { return true }
+	uiThreadID.Store(currentUIThreadID())
+
+	err := runElevatedManifestNative("/tmp/manifest.json", "abc123", "Provision VM")
+	if err == nil {
+		t.Fatal("runElevatedManifestNative succeeded on UI thread")
+	}
+	if !strings.Contains(err.Error(), "cannot run on the app ui thread") {
+		t.Fatalf("error = %v, want UI thread refusal", err)
+	}
+}
+
 func stubAuthorizationCreate(t *testing.T, promptVisible bool) func() {
 	t.Helper()
 
@@ -117,6 +150,53 @@ func stubAuthorizationCreate(t *testing.T, promptVisible bool) func() {
 		return 0
 	}
 	authExecute = func(uintptr, uintptr, uint32, uintptr, *uintptr) int32 { return 0 }
+	authFree = func(uintptr, uint32) int32 { return 0 }
+	authCreateNoUITimeout = 5 * time.Millisecond
+	authCreatePromptTimeout = 5 * time.Millisecond
+	authCreatePollInterval = time.Millisecond
+	authorizationPromptVisible = func() bool { return promptVisible }
+	authorizationStdinIsTTY = func() bool { return true }
+
+	return func() {
+		close(block)
+		<-finished
+		authInitialized = oldAuthInitialized
+		authCreate = oldAuthCreate
+		authExecute = oldAuthExecute
+		authFree = oldAuthFree
+		authCreateNoUITimeout = oldNoUITimeout
+		authCreatePromptTimeout = oldPromptTimeout
+		authCreatePollInterval = oldPollInterval
+		authorizationPromptVisible = oldPromptVisible
+		authorizationStdinIsTTY = oldTTY
+	}
+}
+
+func stubAuthorizationExecute(t *testing.T, promptVisible bool) func() {
+	t.Helper()
+
+	oldAuthInitialized := authInitialized
+	oldAuthCreate := authCreate
+	oldAuthExecute := authExecute
+	oldAuthFree := authFree
+	oldNoUITimeout := authCreateNoUITimeout
+	oldPromptTimeout := authCreatePromptTimeout
+	oldPollInterval := authCreatePollInterval
+	oldPromptVisible := authorizationPromptVisible
+	oldTTY := authorizationStdinIsTTY
+
+	block := make(chan struct{})
+	finished := make(chan struct{})
+	authInitialized = true
+	authCreate = func(_ uintptr, _ uintptr, _ uint32, authRef *uintptr) int32 {
+		*authRef = 42
+		return 0
+	}
+	authExecute = func(uintptr, uintptr, uint32, uintptr, *uintptr) int32 {
+		defer close(finished)
+		<-block
+		return 0
+	}
 	authFree = func(uintptr, uint32) int32 { return 0 }
 	authCreateNoUITimeout = 5 * time.Millisecond
 	authCreatePromptTimeout = 5 * time.Millisecond
