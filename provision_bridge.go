4e874c47f5c9f80c4307d0b23bc1feb1a7cef0c0
Travis Cline <travis.cline@gmail.com>
2026-05-06 23:17:51 -0700

    provision: extract staging helpers to internal package

---
+++ provision_bridge.go
@@ -0,0 +1,73 @@
+package main
+
+import (
+	"fmt"
+	"os"
+
+	"github.com/tmc/vz-macos/internal/provision"
+)
+
+// Provision types are defined in internal/provision and re-exported here.
+type ProvisionConfig = provision.ProvisionConfig
+type InjectOptions = provision.InjectOptions
+type ProvisionManifest = provision.ProvisionManifest
+type ProvisionManifestFile = provision.ProvisionManifestFile
+type stagingFingerprint = provision.StagingFingerprint
+
+func stageFile(stagingDir, relativePath string, data []byte, mode os.FileMode, owner string, manifest *ProvisionManifest) error {
+	if err := provision.StageFile(stagingDir, relativePath, data, mode, owner, manifest); err != nil {
+		return err
+	}
+	if verbose {
+		fmt.Printf("  staged: %s\n", relativePath)
+	}
+	return nil
+}
+
+func writeManifest(stagingDir string, manifest *ProvisionManifest) error {
+	return provision.WriteManifest(stagingDir, manifest)
+}
+
+func readManifest(stagingDir string) (*ProvisionManifest, error) {
+	return provision.ReadManifest(stagingDir)
+}
+
+func makeStagingFingerprint(opts InjectOptions) stagingFingerprint {
+	return provision.MakeStagingFingerprint(opts)
+}
+
+func writeStagingFingerprint(stagingDir string, fp stagingFingerprint) error {
+	return provision.WriteStagingFingerprint(stagingDir, fp)
+}
+
+func stagingMatchesOptions(stagingDir string, opts InjectOptions) (bool, error) {
+	return provision.StagingMatchesOptions(stagingDir, opts)
+}
+
+func readStagingFingerprint(stagingDir string) (stagingFingerprint, bool) {
+	return provision.ReadStagingFingerprint(stagingDir)
+}
+
+func validateUsername(username string) error {
+	return provision.ValidateUsername(username)
+}
+
+func shellEscape(s string) string {
+	return provision.ShellEscape(s)
+}
+
+func manifestNeedsRootProvisioning(manifest *ProvisionManifest) bool {
+	return provision.ManifestNeedsRootProvisioning(manifest, autoLoginLaunchDaemonRelativePath)
+}
+
+func manifestIncludesAgent(manifest *ProvisionManifest) bool {
+	return provision.ManifestIncludesAgent(manifest, agentBinaryName, agentLaunchDaemonLabel, agentLaunchAgentLabel)
+}
+
+func manifestIncludesLoginScreenCredentials(manifest *ProvisionManifest) bool {
+	return provision.ManifestIncludesLoginScreenCredentials(manifest)
+}
+
+func rootWheelVerifyTargets(manifest *ProvisionManifest, mountPoint string) []string {
+	return provision.RootWheelVerifyTargets(manifest, mountPoint)
+}
