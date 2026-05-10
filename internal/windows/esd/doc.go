1fb00f055444f62cbc956874960cd9de8304c484
Travis Cline <travis.cline@gmail.com>
2026-05-10 14:59:23 -0700

    internal/windows/esd: add package doc

---
+++ internal/windows/esd/doc.go
@@ -0,0 +1,5 @@
+// Package esd selects and downloads Windows Electronic Software Download images.
+//
+// It parses the Microsoft Windows catalog, chooses the requested language and
+// architecture, caches complete downloads, and verifies size and SHA-1 metadata.
+package esd
