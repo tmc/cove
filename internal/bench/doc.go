6d7749981752669a3a626d6fefcd1ce8d48caa52
Travis Cline <travis.cline@gmail.com>
2026-05-10 14:59:23 -0700

    internal/bench: add package doc

---
+++ internal/bench/doc.go
@@ -0,0 +1,5 @@
+// Package bench builds cove benchmark reports from checked-in run artifacts.
+//
+// It normalizes cove measurements and unavailable competitor rows into JSON,
+// Markdown, and run-metrics outputs used by release and readiness checks.
+package bench
