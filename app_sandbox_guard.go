329ba68863b6c2a7b6492684620cb212f7b29dff
Travis Cline <travis.cline@gmail.com>
2026-05-27 13:27:29 -0700

    sandbox: consume VM bookmarks

--- app_sandbox_guard.go
+++ app_sandbox_guard.go
@@ -6,6 +6,7 @@ import (
 )
 
 var errAppleAppSandboxHostAccessDenied = errors.New("apple app sandbox denies ambient host access")
+var errPowerboxGrantRequired = errors.New("apple app sandbox requires a Powerbox or bookmark grant")
 
 func appleAppSandboxActive() bool {
 	return currentAppleAppSandboxStatus().Active
@@ -16,4 +17,8 @@ func denyAppleAppSandboxHostAccess(action string) error {
 		return nil
 	}
 	return fmt.Errorf("%s: %w; use COVE_STATE_DIR for VM state or run outside the app sandbox until Powerbox grants are implemented", action, errAppleAppSandboxHostAccessDenied)
+}
+
+func powerboxGrantRequired(action, key, storePath string) error {
+	return fmt.Errorf("%s: %w for %s in %s", action, errPowerboxGrantRequired, key, storePath)
 }
